2f5052909a4e1c2a1ff2b35850442d3a2deeaa12
kerenlu1 <keren.luzon@mail.huji.ac.il>
2017-03-14 15:45:19 +0200

    ex_1 - src files

--- EX_1/src/main/java/com/kerenlu/myapplication_ex_1/MainActivity.java
+++ EX_1/src/main/java/com/kerenlu/myapplication_ex_1/MainActivity.java
@@ -4,26 +4,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.kerenlu.myapplication_ex_1.MESSAGE";
+    Button send;
+    EditText txt;
+    ListView list;
+    ArrayList<String> addArray = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        txt = (EditText)findViewById(R.id.editText);
+        list = (ListView)findViewById(R.id.textList);
+        send = (Button)findViewById(R.id.button);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /** Called when the user taps the Send button */
+            public void onClick(View v) {
+                String getInput = txt.getText().toString();
+                if(getInput == null || getInput.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "Input Field Is Empty", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    addArray.add(getInput);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, addArray);
+                    list.setAdapter(adapter);
+                    txt.setText(" ");
+                }
+            }
+        });
     }
 }
